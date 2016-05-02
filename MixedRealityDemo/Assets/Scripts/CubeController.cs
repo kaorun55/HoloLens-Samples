a7672c7380fedd58a71e54dc0a2a2bcf9e6ccbbb
Kaoru NAKAMURA <kaorun55@gmail.com>
2016-05-03 02:25:32 +0900

    プロジェクトを追加

---
+++ MixedRealityDemo/Assets/Scripts/CubeController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < -3)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ MixedRealityDemo/Assets/Scripts/CubeCreator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CubeCreator : MonoBehaviour {
+    public GameObject cubePrefab;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(CreateCube());
+	}
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    private IEnumerator CreateCube()
+    {
+        while (true)
+        {
+            // カメラの正面から落とす
+            float r = 1.5f;
+            var theta = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+            var x = r * Mathf.Sin(theta);
+            var z = r * Mathf.Cos(theta);
+
+            Instantiate(cubePrefab,
+                new Vector3(x, 1, z),
+                Quaternion.Euler(0, transform.rotation.eulerAngles.y, z));
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
