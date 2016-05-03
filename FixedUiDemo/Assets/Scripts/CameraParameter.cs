a0c3fe1e1e960b5fcadaabf6beac7c6a86177ecd
Kaoru NAKAMURA <kaorun55@gmail.com>
2016-05-03 14:02:50 +0900

    カメラ位置を表示するサンプルを追加

---
+++ FixedUiDemo/Assets/Scripts/CameraParameter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraParameter : MonoBehaviour {
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var position = string.Format("{0:0.000}, {1:0.000}, {2:0.000}", transform.position.x, transform.position.y, transform.position.z);
+        var rotation = string.Format("{0:0.0}, {1:0.0}, {2:0.0}", transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+        text.text = string.Format("{0}\n{1}", position, rotation);
+
+    }
+}
