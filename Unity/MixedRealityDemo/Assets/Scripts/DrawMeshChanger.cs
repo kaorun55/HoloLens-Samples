6eda2fb91404bd7844687f05681970e35e75b05b
Kaoru NAKAMURA <kaorun55@gmail.com>
2016-05-15 07:07:21 +0900

    メッシュをタップで切り替え

---
+++ Unity/MixedRealityDemo/Assets/Scripts/DrawMeshChanger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR.WSA.Input;
+
+public class DrawMeshChanger : MonoBehaviour {
+    GestureRecognizer recognizer;
+
+    public SpatialMapping mapping;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("DrawMeshChanger.Start");
+
+        recognizer = new GestureRecognizer();
+        recognizer.TappedEvent += Recognizer_TappedEvent;
+        recognizer.StartCapturingGestures();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    private void Recognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        Debug.Log(string.Format("Recognizer_TappedEvent : {0}", mapping.DrawVisualMeshes));
+
+        mapping.DrawVisualMeshes = !mapping.DrawVisualMeshes;
+    }
+}
