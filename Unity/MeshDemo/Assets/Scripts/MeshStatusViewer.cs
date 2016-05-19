15d58f13bb040e1ff43d4a5c69571007021d1083
Kaoru NAKAMURA <kaorun55@gmail.com>
2016-05-20 00:54:55 +0900

    Meshのデモを追加

---
+++ Unity/MeshDemo/Assets/Scripts/MeshStatusViewer.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.VR.WSA.Input;
+using UnityEngine.VR.WSA;
+
+public class MeshStatusViewer : MonoBehaviour {
+    SpatialMapping mapping;
+    GestureRecognizer recognizer;
+
+    public Text text;
+
+    float duration = 0;
+
+    public GameObject[] cubes;
+
+    public Vector3 min = Vector3.zero;
+    public Vector3 max = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+        mapping = gameObject.GetComponent<SpatialMapping>();
+        mapping.OnUpdateMesh += Mapping_OnUpdateMesh;
+
+        recognizer = new GestureRecognizer();
+        recognizer.TappedEvent += Recognizer_TappedEvent;
+        recognizer.StartCapturingGestures();
+    }
+
+    private void Recognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        mapping.DrawVisualMeshes = !mapping.DrawVisualMeshes;
+    }
+
+    private void Mapping_OnUpdateMesh(SpatialMapping mapping, SurfaceData bakedData)
+    {
+        text.text = "";
+        text.text += string.Format("Mesh Count  : {0}\n", mapping.surfaces.Count);
+
+        var filter = bakedData.outputMesh;
+        foreach (var vertex in filter.mesh.vertices)
+        {
+            min.x = Mathf.Min(vertex.x, min.x);
+            min.y = Mathf.Min(vertex.y, min.y);
+            min.z = Mathf.Min(vertex.z, min.z);
+
+            max.x = Mathf.Max(vertex.x, max.x);
+            max.y = Mathf.Max(vertex.y, max.y);
+            max.z = Mathf.Max(vertex.z, max.z);
+        }
+
+        text.text += string.Format("Min Point  : {0},{1},{2}\n", min.x, min.y, min.z);
+        text.text += string.Format("Max Point  : {0},{1},{2}\n", max.x, max.y, max.z);
+
+        var tree = ShowHierarchyTree.GetHierarchy();
+        Debug.Log(tree);
+        text.text += tree;
+
+        cubes[0].transform.localPosition = new Vector3(min.x, min.y, min.z);
+        cubes[1].transform.localPosition = new Vector3(min.x, min.y, max.z);
+        cubes[2].transform.localPosition = new Vector3(max.x, min.y, max.z);
+        cubes[3].transform.localPosition = new Vector3(max.x, min.y, min.z);
+
+        cubes[4].transform.localPosition = new Vector3(min.x, max.y, min.z);
+        cubes[5].transform.localPosition = new Vector3(min.x, max.y, max.z);
+        cubes[6].transform.localPosition = new Vector3(max.x, max.y, max.z);
+        cubes[7].transform.localPosition = new Vector3(max.x, max.y, min.z);
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+}
---
+++ Unity/MeshDemo/Assets/Scripts/ShowHierarchyTree.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ShowHierarchyTree {
+    // Use this for initialization
+    public static string GetHierarchy()
+    {
+        string tree = "";
+        // typeで指定した型の全てのオブジェクトを配列で取得し,その要素数分繰り返す.
+        foreach (GameObject obj in UnityEngine.Object.FindObjectsOfType(typeof(GameObject)))
+        {
+            // シーン上に存在するオブジェクト、rootオブジェクト
+            if (obj.activeInHierarchy && (obj == obj.transform.root.gameObject))
+            {
+                // GameObjectの名前を表示.
+                tree += "+" + obj.name + "\n";
+                tree += FindChild(obj, "++");
+            }
+        }
+
+        return tree;
+    }
+
+    private static string FindChild(GameObject obj, string indent)
+    {
+        string tree = "";
+        foreach (Transform child in obj.transform)
+        {
+            tree += indent + child.name + "\n";
+            tree += FindChild(child.gameObject, indent + "+");
+        }
+
+        return tree;
+    }
+}
