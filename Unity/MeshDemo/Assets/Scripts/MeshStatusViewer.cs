7c63429183341ed15659a4c1ef8cd4b0ad8a325c
Kaoru NAKAMURA <kaorun55@gmail.com>
2016-06-01 00:17:00 +0900

    メッシュデータの取り出し

--- Unity/MeshDemo/Assets/Scripts/MeshStatusViewer.cs
+++ Unity/MeshDemo/Assets/Scripts/MeshStatusViewer.cs
@@ -5,11 +5,15 @@ using System.Collections.Generic;
 using UnityEngine.VR.WSA.Input;
 using UnityEngine.VR.WSA;
 using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Linq;
 
 #if !UNITY_EDITOR
 using Windows.Storage;
-using Windows.System;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 #endif
 
 public class MeshStatusViewer : MonoBehaviour {
@@ -19,6 +23,8 @@ public class MeshStatusViewer : MonoBehaviour {
     public Text text;
 
     public GameObject[] cubes;
+
+    bool isSendMesh = false;
 
     // Use this for initialization
     void Start () {
@@ -77,13 +83,15 @@ public class MeshStatusViewer : MonoBehaviour {
             cubes[6].transform.localPosition = new Vector3(max.x, max.y, max.z);
             cubes[7].transform.localPosition = new Vector3(max.x, max.y, min.z);
 
+            SendMesh();
+
 #if !UNITY_EDITOR
             var ao = KnownFolders.PicturesLibrary.CreateFileAsync("mapping.jpg", CreationCollisionOption.ReplaceExisting);
             ao.Completed = async delegate {
                 if (ao.Status == Windows.Foundation.AsyncStatus.Completed)
                 {
                     var file = ao.GetResults();
-                    Debug.Log(file.Name);
+                    //Debug.Log(file.Name);
 
                     using (var stream = await file.OpenAsync(FileAccessMode.ReadWrite))
                     {
@@ -91,7 +99,7 @@ public class MeshStatusViewer : MonoBehaviour {
                         await stream.WriteAsync(imageBuffer.AsBuffer());
                     }
 
-                    Debug.Log("write comlete");
+                    //Debug.Log("write comlete");
                 }
                 else if(ao.Status == Windows.Foundation.AsyncStatus.Error)
                 {
@@ -113,9 +121,88 @@ public class MeshStatusViewer : MonoBehaviour {
     private void Recognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
     {
         mapping.DrawVisualMeshes = !mapping.DrawVisualMeshes;
+
+        isSendMesh = true;
     }
 
     // Update is called once per frame
     void Update () {
+    }
+
+    void SendMesh()
+    {
+        if (!isSendMesh)
+        {
+            return;
+        }
+
+        isSendMesh = false;
+
+        string vertexData = "";
+        string triangleData = "";
+
+        var filters = mapping.GetMeshFilters();
+        int offset = 0;
+        //var filter = filters.First();
+        foreach (var filter in filters)
+        {
+            if (filter != null)
+            {
+                var mesh = filter.sharedMesh;
+
+                foreach (var vertex in mesh.vertices)
+                {
+                    // ローカル座標をワールド座標に変換する
+                    var v = filter.transform.TransformPoint(vertex);
+                    vertexData += string.Format("v {0} {1} {2}\n", v.x, v.y, v.z);
+                }
+
+                for (int i = 0; i < mesh.triangles.Length; i += 3)
+                {
+                    triangleData += string.Format("f {0} {1} {2}\n",
+                        mesh.triangles[i + 0] + offset, mesh.triangles[i + 1] + offset, mesh.triangles[i + 2] + offset);
+                }
+
+                offset = mesh.vertices.Length;
+            }
+        }
+
+        vertexData += triangleData;
+
+
+        Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+        args.RemoteEndPoint = new IPEndPoint(IPAddress.Parse("192.168.11.26"), 44444);
+        args.Completed += (s, e) =>
+        {
+            if (e.ConnectSocket != null)
+            {
+                var data = Encoding.UTF8.GetBytes(vertexData);
+                var length = BitConverter.GetBytes(data.Length);
+
+                SocketAsyncEventArgs sendHeaderArgs = new SocketAsyncEventArgs();
+                sendHeaderArgs.Completed += (ss, ee) => {
+                    Debug.Log("Send Header");
+                    SocketAsyncEventArgs sendDataArgs = new SocketAsyncEventArgs();
+                    sendDataArgs.Completed += (sss, eee) => {
+                        Debug.Log("Complete");
+                    };
+
+                    sendDataArgs.SetBuffer(data, 0, data.Length);
+                    socket.SendAsync(sendDataArgs);
+                };
+                sendHeaderArgs.SetBuffer(length, 0, length.Length);
+                socket.SendAsync(sendHeaderArgs);
+
+
+                Debug.Log("Success");
+            }
+            else
+            {
+                Debug.Log("Error");
+            }
+        };
+        Debug.Log("Connect");
+        socket.ConnectAsync(args);
     }
 }
---
+++ Unity/MeshDemo/External/DataReciever/DataReciever/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataReciever
+{
+    class Program
+    {
+        class App
+        {
+            public void Run()
+            {
+                var listener = new TcpListener(IPAddress.Any, 44444);
+                listener.Start();
+                Console.WriteLine("listener.Start");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    Console.WriteLine("AcceptTcpClient");
+
+                    using (var stream = client.GetStream())
+                    {
+                        var size = new byte[4];
+                        var count = stream.Read(size, 0, size.Length);
+                        var length = BitConverter.ToInt32(size, 0);
+                        Console.WriteLine(string.Format("size({0}) : {1}", count, length));
+                        if (length == 0)
+                        {
+                            break;
+                        }
+
+                        var data = new byte[length];
+                        count = stream.Read(data, 0, data.Length);
+                        Console.WriteLine(string.Format("data({0})", count));
+
+                        var filename = string.Format("{0}.obj", DateTime.Now.ToString("yyyyMMdd-hhmmss"));
+                        File.WriteAllBytes(filename, data);
+                        client.Close();
+                        Console.WriteLine("Write : " + filename);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            App app = new App();
+            app.Run();
+        }
+    }
+}
