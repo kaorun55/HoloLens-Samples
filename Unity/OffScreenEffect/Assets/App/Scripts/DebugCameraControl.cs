3c1e7bf02ba47c582e651075ab8be371bcc8e29e
Kaoru NAKAMURA <kaorun55@gmail.com>
2016-06-10 23:03:12 +0900

    スクリーン外オブジェクトの追跡を追加

---
+++ Unity/OffScreenEffect/Assets/App/Scripts/DebugCameraControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class DebugCameraControl : MonoBehaviour
+{
+#if UNITY_EDITOR
+    private Vector3 oldPos;
+
+    void Update()
+    {
+        //if (Input.GetMouseButton(1))
+        {
+            // 右クリック中にWASDでカメラ前後左右移動
+            transform.position += transform.forward * Input.GetAxis("Vertical") / 30f;
+            transform.position += transform.right * Input.GetAxis("Horizontal") / 30f;
+
+            // 右クリック中にQEでカメラ上下移動
+            float uppos = 0.0f;
+            if (Input.GetKey(KeyCode.Q)) uppos = -0.01f;
+            if (Input.GetKey(KeyCode.E)) uppos = 0.01f;
+            transform.position += transform.up * uppos;
+
+            // 右ドラッグでカメラ向き回転
+            var diff = Input.mousePosition - oldPos;
+            if (Mathf.Abs(diff.x) < 10f && Mathf.Abs(diff.y) < 10f)
+            {
+                transform.RotateAround(Vector3.up, diff.x / 100f);
+                transform.RotateAround(transform.right, -diff.y / 100f);
+            }
+            oldPos = Input.mousePosition;
+        }
+
+        // スクロールでカメラ前後移動
+        transform.position += transform.forward * Input.GetAxis("Mouse ScrollWheel");
+    }
+#endif
+
+}
---
+++ Unity/OffScreenEffect/Assets/App/Scripts/Tracker.cs
@@ -0,0 +1,62 @@
+﻿// http://tsubakit1.hateblo.jp/entry/2016/04/26/233000
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Tracker : MonoBehaviour
+{
+    [SerializeField]
+    Image icon;
+
+    private Rect rect = new Rect(0, 0, 1, 1);
+
+    public Rect canvasRect;
+
+    Vector2 indRange;
+
+    void Start()
+    {
+        var x = icon.rectTransform.rect.width * 3;
+        var y = icon.rectTransform.rect.height * 3;
+
+        // UIがはみ出ないようにする
+        canvasRect = new Rect(0, 0, Screen.width, Screen.height);
+        canvasRect.Set(
+            canvasRect.x + x,
+            canvasRect.y + y,
+            canvasRect.width - x * 2,
+            canvasRect.height - y * 2
+        );
+    }
+
+    void Update()
+    {
+        var v3Screen = Camera.main.WorldToViewportPoint(transform.position);
+        var dir = Vector3.Normalize(v3Screen);
+        dir.y *= -1f;
+
+        var indPos = new Vector2(indRange.x * dir.x, indRange.y * dir.y);
+        indPos = new Vector2((Screen.width / 2) + indPos.x, (Screen.height / 2) + indPos.y);
+
+        Vector3 pdir = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(indPos.x, indPos.y, transform.position.z));
+        pdir = Vector3.Normalize(pdir);
+
+        var angle = Mathf.Atan2(pdir.x, pdir.y) * Mathf.Rad2Deg;
+
+        var viewport = Camera.main.WorldToViewportPoint(transform.position);
+        if (rect.Contains(viewport))
+        {
+            icon.enabled = false;
+        }
+        else
+        {
+            icon.enabled = true;
+
+            // 画面内で対象を追跡
+            viewport.x = Mathf.Clamp01(viewport.x);
+            viewport.y = Mathf.Clamp01(viewport.y);
+            icon.rectTransform.anchoredPosition = Rect.NormalizedToPoint(canvasRect, viewport);
+            icon.transform.rotation = Quaternion.Euler(0, 0, -angle);
+        }
+    }
+}
