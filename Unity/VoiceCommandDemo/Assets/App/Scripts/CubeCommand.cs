ed8ce486abd9248a82d126e1b2751cb4f75dabb5
Kaoru NAKAMURA <kaorun55@gmail.com>
2016-06-17 13:41:08 +0900

    音声コマンドのデモを追加

---
+++ Unity/VoiceCommandDemo/Assets/App/Scripts/CubeCommand.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeCommand : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnRed()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void OnBlue()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void OnGreen()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void OnReset()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
---
+++ Unity/VoiceCommandDemo/Assets/App/Scripts/VoiceCommand.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Collections.Generic;
+using System.Linq;
+using HoloToolkit.Unity;
+
+public class VoiceCommand : MonoBehaviour {
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    Dictionary<string, string> keywordSendMessage = new Dictionary<string, string>();
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("VoiceCommand Start.");
+
+        keywords.Add("Reset", () =>
+        {
+            Debug.Log("Reset");
+
+            // Call the OnReset method on every descendant object.
+            this.BroadcastMessage("OnReset");
+        });
+
+        // Helloが認識しやすい...
+        keywords.Add("Hello", () =>
+        {
+            Debug.Log("Hello");
+
+            var focusObject = GazeManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Call the OnDrop method on just the focused object.
+                focusObject.SendMessage("OnBlue");
+            }
+        });
+
+        keywords.Add("Red", () =>
+        {
+            Debug.Log("Red");
+
+            var focusObject = GazeManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Call the OnDrop method on just the focused object.
+                focusObject.SendMessage("OnRed");
+            }
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
