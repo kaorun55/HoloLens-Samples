7c7196431babebb7f5a07e4ea104f548e3448a01
Kaoru NAKAMURA <kaorun55@gmail.com>
2016-06-22 11:13:51 +0900

    AirTapのデモを追加

---
+++ Unity/AirTapDemo/Assets/App/Scripts/CubeCommand.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeCommand : MonoBehaviour {
+
+    public Material first;
+    public Material second;
+
+    public void OnSelect()
+    {
+        Debug.Log("OnSelect");
+
+        var material = GetComponent<Renderer>().material;
+        GetComponent<Renderer>().material = (material.color == first.color) ? second : first;
+    }
+}
