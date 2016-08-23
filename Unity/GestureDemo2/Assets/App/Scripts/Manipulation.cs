09b72d6527f025d30a6af7312dfac2a590067df3
Kaoru NAKAMURA <kaorun55@gmail.com>
2016-08-23 16:24:11 +0900

    複数のオブジェクトをつまめるようにした

--- Unity/GestureDemo2/Assets/App/Scripts/Manipulation.cs
+++ Unity/GestureDemo2/Assets/App/Scripts/Manipulation.cs
@@ -9,13 +9,23 @@ public class Manipulation : MonoBehaviour {
 
     Vector3 oldPosition = Vector3.zero;
 
+    bool isHold = false;
+
     // Use this for initialization
     void Start () {
         gestureRecognizer = new GestureRecognizer();
-        gestureRecognizer.SetRecognizableGestures(GestureSettings.ManipulationTranslate);
+        gestureRecognizer.SetRecognizableGestures(GestureSettings.ManipulationTranslate | GestureSettings.Hold);
         gestureRecognizer.ManipulationStartedEvent += GestureRecognizer_ManipulationStartedEvent;
         gestureRecognizer.ManipulationUpdatedEvent += GestureRecognizer_ManipulationUpdatedEvent;
+        gestureRecognizer.ManipulationCompletedEvent += GestureRecognizer_ManipulationCompletedEvent;
+        gestureRecognizer.ManipulationCanceledEvent += GestureRecognizer_ManipulationCompletedEvent;
+
         gestureRecognizer.StartCapturingGestures();
+    }
+
+    private void GestureRecognizer_ManipulationCompletedEvent(InteractionSourceKind source, Vector3 cumulativeDelta, Ray headRay)
+    {
+        isHold = false;
     }
 
     // Update is called once per frame
@@ -29,16 +39,26 @@ public class Manipulation : MonoBehaviour {
 
     private void GestureRecognizer_ManipulationUpdatedEvent(InteractionSourceKind source, Vector3 cumulativeDelta, Ray headRay)
     {
-        //Debug.Log("Manipulation Updated : " + string.Format("{0},{1},{2}", cumulativeDelta.x, cumulativeDelta.y, cumulativeDelta.z ));
-        transform.Translate(cumulativeDelta - oldPosition);
+        if (isHold)
+        {
+            //Debug.Log("Manipulation Updated : " + string.Format("{0},{1},{2}", cumulativeDelta.x, cumulativeDelta.y, cumulativeDelta.z ));
+            transform.Translate(cumulativeDelta - oldPosition);
 
-        oldPosition = cumulativeDelta;
+            oldPosition = cumulativeDelta;
+        }
     }
 
     private void GestureRecognizer_ManipulationStartedEvent(InteractionSourceKind source, Vector3 cumulativeDelta, Ray headRay)
     {
         //Debug.Log("Manipulation Started : " + string.Format("{0},{1},{2}", cumulativeDelta.x, cumulativeDelta.y, cumulativeDelta.z));
-        oldPosition = cumulativeDelta;
+        Debug.Log("Manipulation Started : " + gameObject.name);
+
+        if (GestureManager.Instance.FocusedObject == gameObject)
+        {
+            isHold = true;
+
+            oldPosition = cumulativeDelta;
+        }
     }
 }
 
