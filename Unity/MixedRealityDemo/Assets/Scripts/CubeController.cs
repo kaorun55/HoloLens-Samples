7ad81acfbd6cdb85342f2441f8e392e49cd061db
Kaoru NAKAMURA <kaorun55@gmail.com>
2016-09-12 16:30:23 +0900

    HoloTookitを追加

--- Unity/MixedRealityDemo/Assets/Scripts/CubeController.cs
+++ Unity/MixedRealityDemo/Assets/Scripts/CubeController.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CubeController : MonoBehaviour {
+public class CubeController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (transform.position.y < -3)
         {
             Destroy(gameObject);
--- Unity/MixedRealityDemo/Assets/Scripts/CubeCreator.cs
+++ Unity/MixedRealityDemo/Assets/Scripts/CubeCreator.cs
@@ -1,31 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.VR.WSA.Input;
+using System;
 
 public class CubeCreator : MonoBehaviour
 {
+    GestureRecognizer gestureRecognizer;
+
     public GameObject cubePrefab;
 
     // Use this for initialization
     void Start()
     {
-        StartCoroutine(CreateCube());
+        gestureRecognizer = new GestureRecognizer();
+        gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap);
+        gestureRecognizer.TappedEvent += GestureRecognizer_TappedEvent;
+        gestureRecognizer.StartCapturingGestures();
     }
 
-    private IEnumerator CreateCube()
+    private void GestureRecognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
     {
-        while (true)
-        {
-            // カメラの正面から落とす
-            float r = 1.5f;
-            var theta = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
-            var x = r * Mathf.Sin(theta);
-            var z = r * Mathf.Cos(theta);
+        CreateCube();
+    }
 
-            Instantiate(cubePrefab,
-                new Vector3(x, 1, z),
-                Quaternion.Euler(0, transform.rotation.eulerAngles.y, z));
+    private void CreateCube()
+    {
+        // カメラの正面から落とす
+        float r = 1.5f;
+        var theta = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+        var x = r * Mathf.Sin(theta);
+        var z = r * Mathf.Cos(theta);
 
-            yield return new WaitForSeconds(1);
-        }
+        Instantiate(cubePrefab,
+            new Vector3(x, 1, z),
+            Quaternion.Euler(0, transform.rotation.eulerAngles.y, z));
     }
 }
